db0c71148e9495ab1c2984a69150385070784cfe
bala-amypo <bala@amypo.in>
2025-12-16 10:28:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/CreditCardRecorder.java
+++ src/main/java/com/example/demo/entity/CreditCardRecorder.java
@@ -1,1 +1,12 @@
-package
+package com.example.demo;
+
+public class CreditCardRecorder{
+    private Long id;
+    private Long userId;
+    private String cardName;
+    private String issuer;
+    private String cardType;
+    private Double annualFee;
+    private String status;
+    private LocalDateTime createdAt;
+}
