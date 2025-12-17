e464aee72fb99bea6d168975b3d5d95e1c467bd6
bala-amypo <bala@amypo.in>
2025-12-17 10:22:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/CreditCardRecordRepository.java
+++ src/main/java/com/example/demo/repository/CreditCardRecordRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.demo1.model.Student;
 
 @Repository
-public interface CreditCardRecorderRepository extends JpaRepository<Student,Integer>{
+public interface CreditCardRecordRepository extends JpaRepository<CreditCardRecord,Integer>{
 
 }
