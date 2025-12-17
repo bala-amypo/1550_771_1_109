d2ce1d40e519f5a789fecd5dc22ba2fd2d8ef5fd
bala-amypo <bala@amypo.in>
2025-12-17 04:10:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/RecommendationRecord.java
+++ src/main/java/com/example/demo/entity/RecommendationRecord.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
@@ -10,4 +10,48 @@ public class RecommendationRecord{
     private Double expectedRewardValue;
     private String calculationDetailsJson;
     private LocalDateTime recommendedAt;
+
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+    public void setPurchaseIntentId(long purchaseIntentId) {
+        this.purchaseIntentId = purchaseIntentId;
+    }
+    public void setRecommendedCardId(long recommendedCardId) {
+        this.recommendedCardId = recommendedCardId;
+    }
+    public void setExpectedRewardValue(Double expectedRewardValue) {
+        this.expectedRewardValue = expectedRewardValue;
+    }
+    public void setCalculationDetailsJson(String calculationDetailsJson) {
+        this.calculationDetailsJson = calculationDetailsJson;
+    }
+    public void setRecommendedAt(LocalDateTime recommendedAt) {
+        this.recommendedAt = recommendedAt;
+    }
+    public long getId() {
+        return id;
+    }
+    public long getUserId() {
+        return userId;
+    }
+    public long getPurchaseIntentId() {
+        return purchaseIntentId;
+    }
+    public long getRecommendedCardId() {
+        return recommendedCardId;
+    }
+    public Double getExpectedRewardValue() {
+        return expectedRewardValue;
+    }
+    public String getCalculationDetailsJson() {
+        return calculationDetailsJson;
+    }
+    public LocalDateTime getRecommendedAt() {
+        return recommendedAt;
+    }
 }
--- src/main/java/com/example/demo/entity/RewardRule.java
+++ src/main/java/com/example/demo/entity/RewardRule.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.entity;
 
 public class RewardRule{
     private Long id;
@@ -7,4 +7,42 @@ public class RewardRule{
     private String rewardType;
     private Double multiplier;
     private boolean active;
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setCardId(Long cardId) {
+        this.cardId = cardId;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setRewardType(String rewardType) {
+        this.rewardType = rewardType;
+    }
+    public void setMultiplier(Double multiplier) {
+        this.multiplier = multiplier;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    public Long getId() {
+        return id;
+    }
+    public Long getCardId() {
+        return cardId;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getRewardType() {
+        return rewardType;
+    }
+    public Double getMultiplier() {
+        return multiplier;
+    }
+    public boolean isActive() {
+        return active;
+    }
+
+    
 }
