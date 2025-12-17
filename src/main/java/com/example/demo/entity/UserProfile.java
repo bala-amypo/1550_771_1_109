fd2d7a0e202f9f112db562ad2f2392d4a45b45ad
bala-amypo <bala@amypo.in>
2025-12-17 04:15:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -1,9 +1,14 @@
-package com.example.demo;
+package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
 public class UserProfile{
+
+    @Id 
+    @GeneratedValue(Strategy=GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable-false)
     private String userId;
     private String fullName;
     private String email;
@@ -11,6 +16,54 @@ public class UserProfile{
     private String role;
     private boolean active;
     private LocalDateTime createdAt;
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
-
+    
 }
