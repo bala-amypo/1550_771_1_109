46ef65ad92f61aea3a5ad450963200d517866889
bala-amypo <bala@amypo.in>
2025-12-17 04:05:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/CreditCardRecorder.java
+++ src/main/java/com/example/demo/entity/CreditCardRecorder.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
@@ -12,7 +12,56 @@ public class CreditCardRecorder{
     private String status;
     private LocalDateTime createdAt;
 
-    public Long getId{
-        
+    public void setId(Long id) {
+        this.id = id;
     }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+    public void setAnnualFee(Double annualFee) {
+        this.annualFee = annualFee;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    
+
+    public Long getId() {
+        return id;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public String getCardName() {
+        return cardName;
+    }
+    public String getIssuer() {
+        return issuer;
+    }
+    public String getCardType() {
+        return cardType;
+    }
+    public Double getAnnualFee() {
+        return annualFee;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    
 }
