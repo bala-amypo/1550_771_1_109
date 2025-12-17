269dc85e54e98d350d77d71879814dddd63f5675
bala-amypo <bala@amypo.in>
2025-12-17 07:50:57 +0000

    User commit

--- src/main/java/com/example/demo/service/CreditCardServie.java
+++ src/main/java/com/example/demo/service/CreditCardServie.java
@@ -6,4 +6,14 @@ findByUserId(String userId);
 getAllUsers();
 updateUserStatus(Long id,boolean active);
 
-CreditCard
+CreditCardService
+addCard(CreditCarRecord card);
+UpdateCard(Lonf id,CreditCardRecord updated);
+getCardsByUser(Long userId)
+getCardById(Long id)
+getAllCards()
+
+RewardRuleService
+createRule(RewardRule rule)
+updateRule(Long id,RewardRule updated)
+getRulesByCard(Long )
