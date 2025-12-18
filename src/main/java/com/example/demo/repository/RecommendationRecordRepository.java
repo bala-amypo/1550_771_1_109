3f1f47846ddf8d6ea581b21864d2304806b41b72
bala-amypo <bala@amypo.in>
2025-12-18 17:21:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/RecommendationRecordRepository.java
+++ src/main/java/com/example/demo/repository/RecommendationRecordRepository.java
@@ -1,11 +1,24 @@
-// package com.example.demo1.repository;
+package com.example.demo1.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+import com.example.demo.entity.RecommendationRecord;
 
-// import com.example.demo1.model.Student;
+@Repository
+public interface RecommendationRecordRepository extends JpaRepository<RecommendationRecord,Long>{
+    List<RecommendationRecord> findByUserId(Long userId);
 
-// @Repository
-// public interface RecommendationRecordRepository extends JpaRepository<RecommendationRecord,Integer>{
+}
+package com.example.demo.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// }
+import com.example.demo.entity.CreditCardRecord;
+
+@Repository
+public interface CreditCardRecordRepository extends JpaRepository<CreditCardRecord, Long> {
+
+    List<CreditCardRecord> findByUserId(Long userId);
+}
