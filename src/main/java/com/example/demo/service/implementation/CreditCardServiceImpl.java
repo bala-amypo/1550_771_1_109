9f77a2ed279d593f3f08c4b0395c67d5ece88a91
bala-amypo <bala@amypo.in>
2025-12-18 07:53:53 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/CreditCardServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/CreditCardServiceImpl.java
@@ -6,15 +6,31 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.model.Student;
-import com.example.demo.repository.StudentRepository;
-import com.example.demo1.service.StudentService;
+import com.example.demo.entity.CreditCardRecord;
+import com.example.demo.repository.CreditCardRecordRepository;
+import com.example.demo.service.CreditCardService;
 
 @Service
-public class StudentServiceImpl implements StudentService{
+public class CreditCardRecordImpl implements CreditCardService{
     @Autowired
-    StudentRepository studentRepository;
-    
+    CreditCardRecordRepository creditCardRecordRepository;
+
+    @Override
+    public CreditCardRecord updatet(int id,Student UpdatedStudent){
+        Optional<Student> optionalStudent = studentRepository. findById(id);
+        if(optionalStudent.isPresent()){
+        Student oldStudent = optionalStudent.get();
+        oldStudent. setName(UpdatedStudent.getName());
+        oldStudent.setEmail(UpdatedStudent.getEmail());
+        oldStudent.setCourse(UpdatedStudent.getCourse());
+        oldStudent.setAge(UpdatedStudent.getAge());
+        return studentRepository.save(oldStudent);
+        }
+        return null;
+    }
+
+
+
     @Override
     public Student createStudent(Student student){
        return studentRepository.save(student);
