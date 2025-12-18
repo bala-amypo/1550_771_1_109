80f359d911cec7674dfc8ba0095afdaba24c8fa1
bala-amypo <bala@amypo.in>
2025-12-18 16:45:06 +0000

    User commit

--- src/main/java/com/example/demo/repository/RewardRuleRepository.java
+++ src/main/java/com/example/demo/repository/RewardRuleRepository.java
@@ -1,11 +1,12 @@
-// package com.example.demo1.repository;
+package com.example.demo.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+import com.example.demo.entity.RewardRuleRepository;
 
-// import com.example.demo1.model.Student;
+@Repository
+public interface RewardRuleRepository extends JpaRepository<RewardRule, Long> {
 
-// @Repository
-// public interface RewardRuleRepository extends JpaRepository<RewardRule,Integer>{
-
-// }
+    List<RewardRule> findById(Long id);
+}
---
+++ src/main/java/com/example/demo/service/implementation/RewardRuleService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service.implementation;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.UserProfile;
+import com.example.demo.repository.UserProfileRepository;
+import com.example.demo.service.UserProfileService;
+
+@Service
+public class UserProfileServiceImpl implements UserProfileService {
+
+    @Autowired
+    UserProfileRepository userProfileRepository;
+
+    @Override
+    public UserProfile createUser(UserProfile profile){
+       return userProfileRepository.save(profile);
+    }
+
+    @Override
+    public UserProfile getUserById(Long id){
+      Optional<UserProfile> optionalUserProfile = userProfileRepository.findById(id);
+      return optionalUserProfile.orElse(null);
+    }
+
+    @Override
+    public UserProfile findByUserId(String userId){
+      return userProfileRepository.findByUserId(userId);
+    }
+
+    @Override
+    public List<UserProfile> getAllUsers(){
+       return userProfileRepository.findAll();
+    }
+    
+    @Override
+    public UserProfile updateUserStatus(Long id, boolean active) {
+        Optional<UserProfile> optionalUser = userProfileRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            UserProfile oldUser = optionalUser.get();
+            oldUser.setActive(active);
+            return userProfileRepository.save(oldUser);
+        }
+        return null;
+    }
+}
