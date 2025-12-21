c9aee99962d1c00464e750cf87cf813479cec086
bala-amypo <bala@amypo.in>
2025-12-21 15:09:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -1,92 +1,113 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "user_profile")
-public class UserProfile{
+public class UserProfile {
 
-    @Id 
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable=false,unique=true)
+
+    @Column(nullable = false, unique = true)
     private String userId;
-    @Column(nullable=false)
+
+    @Column(nullable = false)
     private String fullName;
-    @Column(nullable=false,unique=true)
+
+    @Column(nullable = false, unique = true)
     private String email;
-    @Column(nullable=false)
+
+    @Column(nullable = false)
     private String password;
-    @Column(nullable=false)
+
+    @Column(nullable = false)
     private String role;
-    @Column(nullable=false)
+
+    @Column(nullable = false)
     private boolean active;
-    @Column(nullable=false)
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
-    public UserProfile(){
-
+    public UserProfile() {
     }
-    
+
     @PrePersist
     public void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
 
-
     public void setId(Long id) {
         this.id = id;
     }
+
     public void setUserId(String userId) {
         this.userId = userId;
     }
+
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public void setRole(String role) {
         this.role = role;
     }
+
     public void setActive(boolean active) {
         this.active = active;
     }
+
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
 
-    
     public Long getId() {
         return id;
     }
+
     public String getUserId() {
         return userId;
     }
+
     public String getFullName() {
         return fullName;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public String getRole() {
         return role;
     }
+
     public boolean isActive() {
         return active;
     }
+
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
-
-    
 }
