a9138e7f1c285b8de2c7df973a31ae2761e732c7
bala-amypo <bala@amypo.in>
2025-12-21 14:34:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/CreditCardRecord.java
+++ src/main/java/com/example/demo/entity/CreditCardRecord.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class CreditCardRecord{
     @Id
+    @GeneratedValue(strategy
     private Long id;
     private Long userId;
     private String cardName;
