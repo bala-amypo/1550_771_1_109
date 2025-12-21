43c8d768f538714d68cbeb70ec1274d3f3ca98a1
bala-amypo <bala@amypo.in>
2025-12-21 16:20:25 +0000

    User commit

--- src/main/java/com/example/demo/service/UserProfileService.java
+++ src/main/java/com/example/demo/service/UserProfileService.java
@@ -1,13 +1,8 @@
 package com.example.demo.service;
 
-import java.util.List;
-
 import com.example.demo.entity.UserProfile;
 
 public interface UserProfileService {
-    UserProfile createUser(UserProfile profile);
-    UserProfile getUserById(Long id);
-    UserProfile findByUserId(String userId);
-    List<UserProfile> getAllUsers();  // changed return type to List<UserProfile>
-    UserProfile updateUserStatus(Long id, boolean active);
+    UserProfile register(UserProfile user);
+    UserProfile findByEmail(String email);
 }
