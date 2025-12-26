0609a6e62124996799c294429d5dd4bd832b3039
bala-amypo <bala@amypo.in>
2025-12-26 09:58:04 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,14 @@
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -12,9 +17,20 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info().title("My API").version("1.0"))
                 .servers(List.of(
                         new Server().url("https://9074.pro604cr.amypo.ai")
-                ));
-        }
+                ))
+                // Adds the global security requirement
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                // Defines the security scheme
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP) // CHANGED THIS LINE
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 }
